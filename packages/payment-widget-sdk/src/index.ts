4352b624fb0e1edf1c3febf16937da389cd1b0a6
Jisoo <jisoo.youn@toss.im>
2022-11-02 18:09:19 +0900

    feat(payment-widget-sdk): ANONYMOUS 사용할 수 있도록 추가

--- packages/payment-widget-sdk/src/index.ts
+++ packages/payment-widget-sdk/src/index.ts
@@ -1,3 +1,4 @@
 export type { PaymentWidgetInstance } from '@tosspayments/payment-widget__types';
 export { loadPaymentWidget } from './loadPaymentWidget';
 export { clearPaymentWidget } from './clearPaymentWidget';
+export const ANONYMOUS = '@@ANONYMOUS';
