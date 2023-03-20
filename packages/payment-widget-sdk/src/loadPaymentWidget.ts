0c2fa172a7594ebc3bdfba96195cf98c0c97aecd
김하영 <hayoung.kim@tosspayments.com>
2023-03-20 21:11:56 +0900

    feat(payment-widget-sdk): 결제위젯 옵션 추가, 의존성 추가

--- packages/payment-widget-sdk/src/loadPaymentWidget.ts
+++ packages/payment-widget-sdk/src/loadPaymentWidget.ts
@@ -1,4 +1,7 @@
-import type { PaymentWidgetConstructor, PaymentWidgetInstance } from '@tosspayments/payment-widget__types';
+import type {
+  PaymentWidgetConstructor,
+  PaymentWidgetInstance,
+} from '@tosspayments/payment-widget__types';
 import { loadScript } from '@tosspayments/sdk-loader';
 import { SCRIPT_URL } from './constants';
 
@@ -7,6 +10,7 @@ type PaymentWidgetParams = Parameters<PaymentWidgetConstructor>;
 export function loadPaymentWidget(
   clientKey: PaymentWidgetParams[0],
   customerKey: PaymentWidgetParams[1],
+  options: PaymentWidgetParams[2],
   loadOptions: { src?: string } = {}
 ): Promise<PaymentWidgetInstance> {
   const { src = SCRIPT_URL } = loadOptions;
@@ -18,6 +22,6 @@ export function loadPaymentWidget(
 
   // regenerator-runtime 의존성을 없애기 위해 `async` 키워드를 사용하지 않는다
   return loadScript<PaymentWidgetConstructor>(src, 'PaymentWidget').then((PaymentWidget) => {
-    return PaymentWidget(clientKey, customerKey);
+    return PaymentWidget(clientKey, customerKey, options);
   });
 }
