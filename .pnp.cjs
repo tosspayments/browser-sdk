260e35d2f5e116c47e9b45f29081c5d84385b6aa
Jisoo <jisoo.youn@toss.im>
2023-03-24 20:27:05 +0900

    fix ci

--- .pnp.cjs
+++ .pnp.cjs
@@ -4903,7 +4903,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@rollup/plugin-babel", "virtual:94dcca1178955513be13f4b9d727bbeb07f95c95127beddcf6bc52d7edd2f452df7bf72547358631aba2726c5c8f9f6f7433337bb1c14895a54bc1c21a35f930#npm:5.3.0"],
             ["@rollup/plugin-commonjs", "virtual:94dcca1178955513be13f4b9d727bbeb07f95c95127beddcf6bc52d7edd2f452df7bf72547358631aba2726c5c8f9f6f7433337bb1c14895a54bc1c21a35f930#npm:21.0.1"],
             ["@rollup/plugin-typescript", "virtual:94dcca1178955513be13f4b9d727bbeb07f95c95127beddcf6bc52d7edd2f452df7bf72547358631aba2726c5c8f9f6f7433337bb1c14895a54bc1c21a35f930#npm:8.3.0"],
-            ["@tosspayments/payment__types", "npm:1.58.1"],
+            ["@tosspayments/payment__types", "npm:1.59.0"],
             ["@tosspayments/sdk-loader", "workspace:packages/sdk-loader"],
             ["@types/jest", "npm:27.0.3"],
             ["jest", "virtual:94dcca1178955513be13f4b9d727bbeb07f95c95127beddcf6bc52d7edd2f452df7bf72547358631aba2726c5c8f9f6f7433337bb1c14895a54bc1c21a35f930#npm:27.3.1"],
@@ -4975,10 +4975,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["@tosspayments/payment__types", [
-        ["npm:1.58.1", {
-          "packageLocation": "./.yarn/cache/@tosspayments-payment__types-npm-1.58.1-550ea78d2b-75cc79dd7d.zip/node_modules/@tosspayments/payment__types/",
+        ["npm:1.59.0", {
+          "packageLocation": "./.yarn/cache/@tosspayments-payment__types-npm-1.59.0-8c01dba6ce-cf56144905.zip/node_modules/@tosspayments/payment__types/",
           "packageDependencies": [
-            ["@tosspayments/payment__types", "npm:1.58.1"],
+            ["@tosspayments/payment__types", "npm:1.59.0"],
             ["@tosspayments/sdk-constants", "npm:0.2.1"]
           ],
           "linkType": "HARD",
