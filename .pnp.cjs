648e8117f726ffe7e2ef564b50bb03ef461ae260
Jiho Shin <ziho@toss.im>
2023-07-14 14:36:03 +0900

    fix(payment-sdk): 해외간편결제 products 타입 수정

--- .pnp.cjs
+++ .pnp.cjs
@@ -4923,7 +4923,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@rollup/plugin-babel", "virtual:94dcca1178955513be13f4b9d727bbeb07f95c95127beddcf6bc52d7edd2f452df7bf72547358631aba2726c5c8f9f6f7433337bb1c14895a54bc1c21a35f930#npm:5.3.0"],
             ["@rollup/plugin-commonjs", "virtual:94dcca1178955513be13f4b9d727bbeb07f95c95127beddcf6bc52d7edd2f452df7bf72547358631aba2726c5c8f9f6f7433337bb1c14895a54bc1c21a35f930#npm:21.0.1"],
             ["@rollup/plugin-typescript", "virtual:94dcca1178955513be13f4b9d727bbeb07f95c95127beddcf6bc52d7edd2f452df7bf72547358631aba2726c5c8f9f6f7433337bb1c14895a54bc1c21a35f930#npm:8.3.0"],
-            ["@tosspayments/payment__types", "npm:1.62.2"],
+            ["@tosspayments/payment__types", "npm:1.64.2"],
             ["@tosspayments/sdk-loader", "workspace:packages/sdk-loader"],
             ["@types/jest", "npm:27.0.3"],
             ["jest", "virtual:94dcca1178955513be13f4b9d727bbeb07f95c95127beddcf6bc52d7edd2f452df7bf72547358631aba2726c5c8f9f6f7433337bb1c14895a54bc1c21a35f930#npm:27.3.1"],
@@ -4997,18 +4997,18 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["@tosspayments/payment__types", [
-        ["npm:1.62.2", {
-          "packageLocation": "./.yarn/cache/@tosspayments-payment__types-npm-1.62.2-afeb8f61ae-e9437cc0bf.zip/node_modules/@tosspayments/payment__types/",
-          "packageDependencies": [
-            ["@tosspayments/payment__types", "npm:1.62.2"],
-            ["@tosspayments/sdk-constants", "npm:0.2.2"]
-          ],
-          "linkType": "HARD",
-        }],
         ["npm:1.63.1", {
           "packageLocation": "./.yarn/cache/@tosspayments-payment__types-npm-1.63.1-f38b097487-d26625b7a0.zip/node_modules/@tosspayments/payment__types/",
           "packageDependencies": [
             ["@tosspayments/payment__types", "npm:1.63.1"],
+            ["@tosspayments/sdk-constants", "npm:0.2.2"]
+          ],
+          "linkType": "HARD",
+        }],
+        ["npm:1.64.2", {
+          "packageLocation": "./.yarn/cache/@tosspayments-payment__types-npm-1.64.2-4e36dec287-a0011aa345.zip/node_modules/@tosspayments/payment__types/",
+          "packageDependencies": [
+            ["@tosspayments/payment__types", "npm:1.64.2"],
             ["@tosspayments/sdk-constants", "npm:0.2.2"]
           ],
           "linkType": "HARD",
