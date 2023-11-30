80b990c92d67ece45ef5f4611eab55715d4d92a6
Ian Park <sh.park@toss.im>
2023-11-30 17:37:27 +0900

    feat(brandpay-sdk): sdk load priority 옵션 추가 (#87)

--- packages/brandpay-sdk/src/loadBrandPay.ts
+++ packages/brandpay-sdk/src/loadBrandPay.ts
@@ -1,24 +1,29 @@
-import type { BrandpayConstructor, BrandPayInstance } from '@tosspayments/brandpay__types';
+import type { BrandpayConstructor, BrandPayInstance as BrandpayInstance } from '@tosspayments/brandpay__types';
 import { loadScript } from '@tosspayments/sdk-loader';
 import { SCRIPT_URL } from './constants';
 
-type BrandPayParams = Parameters<BrandpayConstructor>;
+type BrandpayParams = Parameters<BrandpayConstructor>;
+
+interface LoadOptions {
+  src?: string;
+  network?: Parameters<typeof loadScript>[2];
+}
 
 export function loadBrandPay(
-  clientKey: BrandPayParams[0],
-  customerKey: BrandPayParams[1],
-  options: BrandPayParams[2] = {},
-  loadOptions: { src?: string } = {}
-): Promise<BrandPayInstance> {
-  const { src = SCRIPT_URL } = loadOptions;
+  clientKey: BrandpayParams[0],
+  customerKey: BrandpayParams[1],
+  options: BrandpayParams[2] = {},
+  loadOptions: LoadOptions = {},
+): Promise<BrandpayInstance> {
+  const { src = SCRIPT_URL, network } = loadOptions;
 
   if (typeof window === 'undefined') {
     // XXX(@HyunSeob): SSR할 때 생성자가 사용되는 경우 에러를 발생시키지 않는대신 정상적인 인스턴스를 반환하지 않는다.
-    return Promise.resolve({} as BrandPayInstance);
+    return Promise.resolve({} as BrandpayInstance);
   }
 
   // regenerator-runtime 의존성을 없애기 위해 `async` 키워드를 사용하지 않는다
-  return loadScript<BrandpayConstructor>(src, 'BrandPay').then((BrandPay) => {
+  return loadScript<BrandpayConstructor>(src, 'BrandPay', network).then((BrandPay) => {
     return BrandPay(clientKey, customerKey, options);
   });
 }
