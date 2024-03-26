b4ba5726e594750d328c234fd9407c6bec4221e1
Jisoo <jisoo.youn@toss.im>
2024-03-26 17:03:51 +0900

    fix(payment-sdk): 계좌이체 파라미터 추가

--- .pnp.cjs
+++ .pnp.cjs
@@ -4902,7 +4902,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@rollup/plugin-babel", "virtual:94dcca1178955513be13f4b9d727bbeb07f95c95127beddcf6bc52d7edd2f452df7bf72547358631aba2726c5c8f9f6f7433337bb1c14895a54bc1c21a35f930#npm:5.3.0"],
             ["@rollup/plugin-commonjs", "virtual:94dcca1178955513be13f4b9d727bbeb07f95c95127beddcf6bc52d7edd2f452df7bf72547358631aba2726c5c8f9f6f7433337bb1c14895a54bc1c21a35f930#npm:21.0.1"],
             ["@rollup/plugin-typescript", "virtual:94dcca1178955513be13f4b9d727bbeb07f95c95127beddcf6bc52d7edd2f452df7bf72547358631aba2726c5c8f9f6f7433337bb1c14895a54bc1c21a35f930#npm:8.3.0"],
-            ["@tosspayments/payment__types", "npm:1.67.0"],
+            ["@tosspayments/payment__types", "npm:1.67.2"],
             ["@tosspayments/sdk-loader", "workspace:packages/sdk-loader"],
             ["@types/jest", "npm:27.0.3"],
             ["jest", "virtual:94dcca1178955513be13f4b9d727bbeb07f95c95127beddcf6bc52d7edd2f452df7bf72547358631aba2726c5c8f9f6f7433337bb1c14895a54bc1c21a35f930#npm:27.3.1"],
@@ -4980,6 +4980,14 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/@tosspayments-payment__types-npm-1.67.0-241b2b610c-3c0d40579c.zip/node_modules/@tosspayments/payment__types/",
           "packageDependencies": [
             ["@tosspayments/payment__types", "npm:1.67.0"],
+            ["@tosspayments/sdk-constants", "npm:0.2.2"]
+          ],
+          "linkType": "HARD",
+        }],
+        ["npm:1.67.2", {
+          "packageLocation": "./.yarn/cache/@tosspayments-payment__types-npm-1.67.2-a30904cd96-c426a55680.zip/node_modules/@tosspayments/payment__types/",
+          "packageDependencies": [
+            ["@tosspayments/payment__types", "npm:1.67.2"],
             ["@tosspayments/sdk-constants", "npm:0.2.2"]
           ],
           "linkType": "HARD",
