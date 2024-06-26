010e23d3723dbff6e179af3b75ea9c3f95f32295
moresoo <sshn611@naver.com>
2024-06-26 20:18:00 +0900

    feat: ANONYMOUS PI에 정의한 상수로 내보내기

--- packages/tosspayments-sdk/src/index.ts
+++ packages/tosspayments-sdk/src/index.ts
@@ -1,3 +1,3 @@
 export { loadTossPayments } from './loadTossPayments';
 export { clearTossPayments } from './clearTossPayments';
-export const ANONYMOUS = '@@ANONYMOUS';
+export { ANONYMOUS } from '@tosspayments/standard-public-interfaces';
