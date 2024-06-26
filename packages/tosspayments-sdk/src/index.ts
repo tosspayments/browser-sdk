80095d883853ccf7e27ac73e015864ca627e189e
moresoo <sshn611@naver.com>
2024-06-26 20:30:48 +0900

    chore: ANONYMOUS는 SDK에서 내보내주고 있어서 browser-sdk에서 내보내줄 필요 없음

--- packages/tosspayments-sdk/src/index.ts
+++ packages/tosspayments-sdk/src/index.ts
@@ -1,4 +1,3 @@
-export { ANONYMOUS } from '@tosspayments/standard-public-interfaces';
 export { clearTossPayments } from './clearTossPayments';
 export { loadTossPayments } from './loadTossPayments';
 
