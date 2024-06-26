ab30d2a88e9fe8a98f430dcd6af67bea017eca58
moresoo <sshn611@naver.com>
2024-06-26 17:35:52 +0900

    chore: TossPayment namespace type declare

--- packages/tosspayments-sdk/src/clearPaymentWidget.ts
+++ packages/tosspayments-sdk/src/clearPaymentWidget.ts
@@ -1,4 +1,11 @@
 import { SCRIPT_URL } from './constants';
+import { TossPayments } from '@tosspayments/standard-public-interfaces';
+
+declare global {
+  interface Window {
+    TossPayments?: TossPayments & { version: string; isV2: boolean; ANONYMOUS: string };
+  }
+}
 
 export function clearPaymentWidget() {
   const script = document.querySelector(`script[src="${SCRIPT_URL}"]`);
