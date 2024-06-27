4754c9da2dc890d61ac630dd43b00a79dcf1d484
moresoo <sshn611@naver.com>
2024-06-28 03:25:34 +0900

    feat: script 로드가 완료되면, 주어진 namespace에 생성된 인스턴스와 동일한 인스턴스를 resolve 해야한다

--- packages/sdk-loader/src/loadScript.ts
+++ packages/sdk-loader/src/loadScript.ts
@@ -1,6 +1,12 @@
 export function loadScript<Namespace>(src: string, namespace: string): Promise<Namespace> {
   return new Promise((resolve) => {
-    resolve(null);
+    const script = document.createElement('script');
+    script.src = src;
+    script.addEventListener('load', () => {
+      if (window[namespace]) {
+        resolve(window[namespace]);
+      }
+    });
   });
 
 }
