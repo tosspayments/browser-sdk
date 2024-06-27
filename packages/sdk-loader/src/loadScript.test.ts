901e8185ea7d4550b654a4aba0522c6f49789878
moresoo <sshn611@naver.com>
2024-06-28 04:39:23 +0900

    refactor: 공통 테스트 모킹 로직 beforeEach로 뺌

--- packages/sdk-loader/src/loadScript.test.ts
+++ packages/sdk-loader/src/loadScript.test.ts
@@ -1,71 +1,66 @@
-import { beforeEach, describe, expect, test, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 import { loadScript, clearCache } from './loadScript';
 
 declare global {
   interface Window {
-    [key: string]: any;
+    [TossPayments: string]: any;
   }
 }
 
 describe('loadScript', () => {
+  // NOTE: load, error 이벤트를 임의로 발생시키기 위해 이벤트 리스터를 모킹합니다
+  let eventListeners1: { [key: string]: EventListener };
+  let eventListeners2: { [key: string]: EventListener };
+
   beforeEach(() => {
-    vi.restoreAllMocks();
     document.head.innerHTML = '';
-    document.head.appendChild = vi.fn(); // script가 inject 되는 것을 방지
+    document.head.appendChild = vi.fn(); // NOTE: 테스트 환경에서 script inject 방지
     window.TossPayments = undefined;
+
+    // 캐시 초기화
     clearCache();
+
+    eventListeners1 = {};
+    eventListeners2 = {};
+
+    const script1 = document.createElement('script');
+    const script2 = document.createElement('script');
+
+    script1.addEventListener = (event: string, listener: EventListener) => {
+      eventListeners1[event] = listener;
+    };
+
+    script2.addEventListener = (event: string, listener: EventListener) => {
+      eventListeners2[event] = listener;
+    };
+
+    vi.spyOn(document, 'createElement')
+      .mockReturnValueOnce(script1)
+      .mockReturnValueOnce(script2);
   });
 
   describe('기본 동작', () => {
     test('script 로드가 완료되면, 주어진 namespace에 생성된 인스턴스와 동일한 인스턴스를 resolve 해야한다', async () => {
-      // given
-      const eventListeners: { [key: string]: EventListener } = {};
-      const script = document.createElement('script');
-
-      vi.spyOn(document, 'createElement').mockReturnValue(script);
-
-      script.addEventListener = (event: string, listener: EventListener) => {
-        eventListeners[event] = listener;
-      };
-
       // when
       const promise = loadScript('http://example.com/example.js', 'TossPayments');
       window.TossPayments = {}; // SDK는 주어진 namespace에 인스턴스를 생성함
-      eventListeners.load(new Event('load')); // script 로드가 완료됨
+      eventListeners1.load(new Event('load')); // script 로드가 완료됨
 
       // then
       expect(promise).resolves.toBe(window.TossPayments);
     });
     test('script 로드를 실패하면, 에러를 throw 해야한다', async () => {
-      // given
-      const eventListeners: { [key: string]: EventListener } = {};
-      const script = document.createElement('script');
-      vi.spyOn(document, 'createElement').mockReturnValue(script);
-
-      script.addEventListener = (event: string, listener: EventListener) => {
-        eventListeners[event] = listener;
-      };
-
       // when
       const promise = loadScript('http://example.com/example.js', 'TossPayments');
-      eventListeners.error(new Event('error')); // script 로드가 실패함
+      eventListeners1.error(new Event('error')); // script 로드가 실패함
 
       // then
       expect(promise).rejects.toThrowError('[TossPayments SDK] Failed to load script: [http://example.com/example.js]');
     });
     test('script 로드를 성공했지만 namespace에 인스턴스가 존재하지 않으면, 에러를 throw 해야한다', async () => {
-      // given
-      const eventListeners: { [key: string]: EventListener } = {};
-      const script = document.createElement('script');
-      vi.spyOn(document, 'createElement').mockReturnValue(script);
-
-      script.addEventListener = (event: string, listener: EventListener) => {
-        eventListeners[event] = listener;
-      };
-
       // when
       const promise = loadScript('http://example.com/example.js', 'TossPayments');
-      eventListeners.load(new Event('load')); // script 로드가 완료됨
+      eventListeners1.load(new Event('load')); // script 로드가 완료됨
 
       // then
       expect(promise).rejects.toThrowError('[TossPayments SDK] TossPayments is not available');
@@ -74,32 +69,12 @@ describe('loadScript', () => {
 
   describe('캐시된 script 로더 Promise가 존재하면', () => {
     test('해당 Promise를 반환해야 한다', async () => {
-      // given
-      const eventListeners1: { [key: string]: EventListener } = {};
-      const eventListeners2: { [key: string]: EventListener } = {};
-
-      const script1 = document.createElement('script');
-      const script2 = document.createElement('script');
-
-      script1.addEventListener = (event: string, listener: EventListener) => {
-        eventListeners1[event] = listener;
-      };
-
-      script2.addEventListener = (event: string, listener: EventListener) => {
-        eventListeners2[event] = listener;
-      };
-
-      vi.spyOn(document, 'createElement')
-        .mockReturnValueOnce(script1)
-        .mockReturnValueOnce(script2);
-
       // when
       const promise1 = loadScript('http://example.com/script.js', 'TossPayments');
       window.TossPayments = {}; // SDK는 주어진 namespace에 인스턴스를 생성함
       eventListeners1.load(new Event('load')); // script 로드가 완료됨
 
       const promise2 = loadScript('http://example.com/script.js', 'TossPayments');
-
 
       // then
       expect(promise1).toBe(promise2);
