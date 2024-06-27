a4235ec9d566b736cad39b7e4f174ae95445cef8
moresoo <sshn611@naver.com>
2024-06-28 05:54:39 +0900

    refactor: 코드 가독성

--- packages/sdk-loader/src/loadScript.ts
+++ packages/sdk-loader/src/loadScript.ts
@@ -4,43 +4,54 @@ interface LoadOptions {
   priority?: 'high' | 'low' | 'auto';
 }
 
-
 export function loadScript<Namespace>(src: string, namespace: string, options: LoadOptions = {}): Promise<Namespace> {
   if (cachedPromise != null) {
     return cachedPromise;
   }
 
   cachedPromise = new Promise((resolve, reject) => {
-    if (typeof window === 'undefined' || typeof document === 'undefined') {
-      return resolve(null);
-    }
-
-    if (window[namespace] != null) {
-      return resolve(window[namespace]);
-    }
-
-    const script = document.createElement('script');
-    if (options.priority != null) {
-      (script as any).fetchPriority = options.priority;
-    }
-
-    script.src = src;
-    script.addEventListener('load', () => {
-      if (window[namespace]) {
-        resolve(window[namespace]);
-      } else {
-        reject(new Error(`[TossPayments SDK] ${namespace} is not available`));
+    try {
+      if (typeof window === 'undefined' || typeof document === 'undefined') {
+        return resolve(null);
       }
-    });
 
-    script.addEventListener('error', () => {
-      reject(new Error(`[TossPayments SDK] Failed to load script: [${src}]`))
-    });
+      if (getNamespace(namespace) != null) {
+        return resolve(getNamespace<Namespace>(namespace));
+      }
 
-    document.head.appendChild(script);
+      const script = document.createElement('script');
+      script.src = src;
+      script.addEventListener('load', onLoad);
+      script.addEventListener('error', onError);
+
+      if (options.priority != null) {
+        (script as any).fetchPriority = options.priority;
+      }
+
+      function onLoad() {
+        if (getNamespace(namespace) != null) {
+          resolve(getNamespace<Namespace>(namespace));
+        } else {
+          reject(new Error(`[TossPayments SDK] ${namespace} is not available`));
+        }
+      }
+
+      function onError() {
+        reject(new Error(`[TossPayments SDK] Failed to load script: [${src}]`));
+      }
+
+      document.head.appendChild(script);
+    } catch (error) {
+      reject(error);
+      return;
+    }
   });
 
   return cachedPromise;
+}
+
+function getNamespace<Namespace>(name: string) {
+  return (window[name as any] as any) as Namespace | undefined;
 }
 
 export function clearCache() {
