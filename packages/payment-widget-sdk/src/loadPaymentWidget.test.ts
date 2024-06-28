a9b4eb629dd592a75488edbf3ace54b911423550
moresoo <sshn611@naver.com>
2024-06-28 13:39:15 +0900

    test: v1 브랜드페이, v1 위젯에도 테스트 추가

---
+++ packages/payment-widget-sdk/src/loadPaymentWidget.test.ts
@@ -0,0 +1,52 @@
+import { clearCache, NamespaceNotAvailableError } from '@tosspayments/sdk-loader';
+import { afterEach, describe, expect, test } from 'vitest';
+import { SCRIPT_URL } from './constants';
+import { loadPaymentWidget } from './loadPaymentWidget';
+
+describe('loadTossPayments', () => {
+  afterEach(() => {
+    document.head.innerHTML = '';
+    document.body.innerHTML = '';
+    // @ts-ignore
+    delete window.PaymentWidget;
+
+    clearCache();
+  });
+
+  test('URL이 들어간 <script>를 <head>에 inject한다', async () => {
+    await loadPaymentWidget('test_key', 'customer_key');
+
+    const script = document.querySelector(`script[src="${SCRIPT_URL}"]`);
+
+    expect(script).not.toBeNull();
+  });
+
+  test('2회 이상의 중복 호출 시에도 1회만 inject한다', async () => {
+    await Promise.all(Array(10).fill(loadPaymentWidget('test_key', 'customer_key')));
+
+    const scripts = document.querySelectorAll(`script[src="${SCRIPT_URL}"]`);
+
+    expect(scripts).toHaveLength(1);
+  });
+
+  test(`src를 지정하면 주어진 URL로 script를 로드한다`, async () => {
+    const testSource = `https://js.tosspayments.com/v1/brandpay`;
+
+    try {
+      await loadPaymentWidget('test_key', 'customer_key', {}, {
+        src: testSource,
+      });
+    } catch (error) {
+      if (error instanceof NamespaceNotAvailableError) {
+        // NOTE: SDK에서 namespace에 인스턴스를 꽂아주는 동작이 테스트 환경에서는 일어나지 않아 발생하는 에러를 무시합니다
+        return;
+      }
+
+      throw error;
+    }
+
+    const script = document.querySelector(`script[src="${testSource}"]`);
+
+    expect(script).not.toBeNull();
+  });
+});
