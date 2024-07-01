6ce9a1e6cf2a25ddeeadc36dcfbb1819b2f57305
moresoo <sshn611@naver.com>
2024-07-01 13:55:17 +0900

    refactor: SDK load 테스트에서 NamespaceNotAvailableError 에러 관련 검증을 하지 않는다

--- packages/payment-sdk/src/loadTossPayments.test.ts
+++ packages/payment-sdk/src/loadTossPayments.test.ts
@@ -31,7 +31,6 @@ describe('loadTossPayments', () => {
 
   test('src를 지정하면 주어진 URL로 script를 로드한다', async () => {
     const { loadTossPayments } = await import('./loadTossPayments');
-    const { NamespaceNotAvailableError } = await import('@tosspayments/sdk-loader');
 
     const testSource = `https://js.tosspayments.com/v1/brandpay`;
 
@@ -40,12 +39,7 @@ describe('loadTossPayments', () => {
         src: testSource,
       });
     } catch (error) {
-      if (error instanceof NamespaceNotAvailableError) {
-        // NOTE: SDK에서 namespace에 인스턴스를 꽂아주는 동작이 테스트 환경에서는 일어나지 않아 발생하는 에러를 무시합니다
-        return;
-      }
-
-      throw error;
+      // NOTE: SDK에서 namespace에 인스턴스를 꽂아주는 동작이 테스트 환경에서는 일어나지 않아 발생하는 에러를 무시합니다.
     }
 
     const script = document.querySelector(`script[src="${testSource}"]`);
