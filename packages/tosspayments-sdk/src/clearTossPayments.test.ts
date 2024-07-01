762546989c7f39b83d5344349f60dbb8be2339e5
songsanghyeon <sshn611@naver.com>
2024-07-01 17:31:30 +0900

    fix(@tosspayments-sdk): 사용하는 측에서 타입 추론이 안 되는 이슈를 수정합니다 (#105)
    * fix: 타입추론이 안되는 문제 수정
    
    * chore: revert ci workflow
    
    * test: SDK 로그 -> 네임스페이스 할당 순서 보장 (이미 네임스페이스를 점유하고 있어서 에러 발생)

--- packages/tosspayments-sdk/src/clearTossPayments.test.ts
+++ packages/tosspayments-sdk/src/clearTossPayments.test.ts
@@ -6,13 +6,13 @@ const SCRIPT_ID = `__tosspayments-sdk__`;
 
 describe(`clearTossPayments`, () => {
   test(`토스페이먼츠 SDK 스크립트 태그를 제거하고, 전역에 존재하는 TossPayments 객체를 제거한다`, () => {
-    // @ts-ignore
-    window.TossPayments = vi.fn();
-
     const script = document.createElement(`script`);
     script.src = SCRIPT_URL;
     script.id = `__tosspayments-sdk__`;
     document.head.appendChild(script);
+
+    // @ts-ignore
+    window.TossPayments = vi.fn();
 
     clearTossPayments();
 
