724501c8ad79e395d29b0b11191d6e296d0f8134
Jisoo Youn <yjs930915@gmail.com>
2024-07-08 16:50:33 +0900

    fix(tosspayments-sdk): 위젯 연동시 필요한 type export (#116)
    Co-authored-by: Jisoo <jisoo.youn@toss.im>

--- packages/tosspayments-sdk/src/index.ts
+++ packages/tosspayments-sdk/src/index.ts
@@ -2,3 +2,11 @@ export { clearTossPayments } from './clearTossPayments';
 export { loadTossPayments } from './loadTossPayments';
 
 export const ANONYMOUS = '@@ANONYMOUS';
+
+import { WidgetAgreementStatus, WidgetAgreementWidget, WidgetPaymentMethodWidget, WidgetSelectedPaymentMethod } from '@tosspayments/standard-public-interfaces';
+
+export type { WidgetAgreementStatus,
+  WidgetAgreementWidget,
+  WidgetPaymentMethodWidget,
+  WidgetSelectedPaymentMethod
+};
