4b1601206ba77856a1fe0c811f01a4a8700a9f53
Jisoo Youn <yjs930915@gmail.com>
2024-07-17 14:37:45 +0900

    fix(tosspayments-sdk): loadTossPayments 반환 타입 변경 (#118)
    Co-authored-by: Jisoo <jisoo.youn@toss.im>

--- packages/tosspayments-sdk/src/loadTossPayments.ts
+++ packages/tosspayments-sdk/src/loadTossPayments.ts
@@ -1,18 +1,18 @@
 import { loadScript } from '@tosspayments/sdk-loader';
 import { SCRIPT_URL } from './constants';
-import { TossPayments, TossPaymentsInstance } from '@tosspayments/standard-public-interfaces';
+import { TossPayments, TossPaymentsSDK } from '@tosspayments/standard-public-interfaces';
 
 type TosspaymentsParams = Parameters<TossPayments>;
 
 export function loadTossPayments(
   clientKey: TosspaymentsParams[0],
   loadOptions: { src?: string } = {}
-): Promise<TossPaymentsInstance> {
+): Promise<TossPaymentsSDK> {
   const { src = SCRIPT_URL } = loadOptions;
 
   if (typeof window === 'undefined') {
     // XXX(@HyunSeob): SSR할 때 생성자가 사용되는 경우 에러를 발생시키지 않는대신 정상적인 인스턴스를 반환하지 않는다.
-    return Promise.resolve({} as TossPaymentsInstance);
+    return Promise.resolve({} as TossPaymentsSDK);
   }
 
   // regenerator-runtime 의존성을 없애기 위해 `async` 키워드를 사용하지 않는다
